c9948e2ae9de3138be59726d4a83bcfb65099f97
brian <mairurat@gmail.com>
2023-01-30 12:14:31 +0300

    Nested array destructuring

--- JS/src/arrayDestructure.js
+++ JS/src/arrayDestructure.js
@@ -6,8 +6,12 @@ console.log("Array:", a, b, c);
 const restaurant = {
   name: "Classico Italiano",
   location: ["Italian", "Pizzeria", "Vegeterian", "Organic"],
-  starterMenu: ["Focaccia", "Bruschetta", "Garlic", "Bread", "Salad"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Salad"],
   mainMenu: ["Pizza", "Risotto", "Pasta"],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 };
 
 const [first, second] = restaurant.location;
@@ -25,3 +29,16 @@ console.log("Switch Case:", main, secondary);
 // console.log("Results of switch:",main, secondary);
 [main, secondary] = [secondary, main];
 console.log(main, secondary);
+
+//Order
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+//Nested array
+const nested = [2, 4, [5, 6]];
+const [kwanza, , mwisho] = nested;
+console.log(kwanza, mwisho);
+
+//Each element
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
